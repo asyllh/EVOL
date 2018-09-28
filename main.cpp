e7c0bd9c39ab983cfabc04e9e3d64293b4432457
ALH <asylhawa@rocketmail.com>
2018-09-28 21:11:22 +0100

    added sets feasiblePacking and infeasiblePacking

--- main.cpp
+++ main.cpp
@@ -4,19 +4,9 @@ main.cpp
 Evolutionary Algorithm with Local Search
 23/08/2018
 /--------------*/
-/** TO DO **/
-/* Need to transpose matrix A, currently the rows are items and columns are strips, we need
-   rows to be strips and coulumns to be items for it to be used in DLX
- * Then, the problem is to find the subset of rows (strips) that contains every item
-   exactly once (i.e. every column must have an item in one of the rows)
- * Write out to .txt file the 0-1 matrix A to be used in DLX
- * Need to check if qualityStrips actually contains every item
- * Check cases where item has score widths that will not meet VSC with the largest
-   score width in the set.
- * Need to then add these items in strips to qualityStrips to use in DLX
- * Make first row of dlxMatrix just 0, 1,...., numItems, so that in the output
-   file the first line will be 0,1,... numItems and they can be read in as the
-   column names?
+
+/** NOTES **/
+/* Why does the program take longer when n = 10? (numItems = 10)
  */
 
 #include <iostream>
@@ -35,7 +25,7 @@ int main(int argc, char** argv){
         exit(1);
     }
 
-    int x;
+    //region Default Variables
     int numIterations = 1000;
     int tau = 70;
     int numItem = 500;
@@ -47,10 +37,11 @@ int main(int argc, char** argv){
     int numPop = 0;
     int xOver = 1;
     int randomSeed = 1;
+    //endregion
 
 
     //region User Arguments
-    for(x = 1; x < argc; ++x){
+    for(int x = 1; x < argc; ++x){
         if(strcmp("-i", argv[x]) == 0){
             numIterations = atoi(argv[++x]);
         }
@@ -89,7 +80,8 @@ int main(int argc, char** argv){
 
     ArgumentCheck(numIterations, tau, numItem, minWidth, maxWidth, minItemWidth, maxItemWidth, stripWidth, numPop, xOver, randomSeed);
 
-    int i, j, k, bestStart, bestEnd;
+    //region Variables
+    int bestStart, bestEnd;
     int iteration;
     int numScores = numItem * 2;
     double totalItemWidth;
@@ -107,6 +99,9 @@ int main(int argc, char** argv){
     std::vector<std::vector<int> > qualityStrips;
     std::vector<int> qualityStripsSum;
     std::vector<int> qualityItems(numItem, 0);
+    std::set<std::vector<int> > feasiblePacking;
+    std::set<std::vector<int> > infeasiblePacking;
+    //endregion
 
     srand(randomSeed);
     Timer timer;
@@ -114,16 +109,26 @@ int main(int argc, char** argv){
     CreateInstance(tau, numScores, numItem, minWidth, maxWidth, minItemWidth, maxItemWidth, totalItemWidth, allScores, partners,
                    adjMatrix, itemWidths, allItems);
 
+    std::vector<int> temp;
+    for(int i = 0; i < numItem; ++i){
+        temp.push_back(i);
+        feasiblePacking.insert(temp);
+        temp.clear();
+    }
+
+
+
+
 
     int lowerBound = LowerBound(stripWidth, totalItemWidth);
-    std::cout << "Lower bound: " << lowerBound << " strips.\n\n";
+    //std::cout << "Lower bound: " << lowerBound << " strips.\n\n";
 
     CreateInitPop(tau, numPop, numScores, numItem, maxItemWidth, stripWidth, allScores, partners, adjMatrix, itemWidths,
-                  populationSum, population);
+                  populationSum, population, allItems, feasiblePacking);
 
 
     //Finding the solution in the population that has the best fitness value
-    for(i = 0; i < population.size(); ++i){
+    for(int i = 0; i < population.size(); ++i){
         tempFitness = Fitness(stripWidth, populationSum[i], population[i]);
         if(tempFitness > bestFitness){
             bestFitness = tempFitness;
@@ -134,8 +139,8 @@ int main(int argc, char** argv){
     bestSolnStartSum = populationSum[bestStart];
 
 
-    std::cout << "START - Best solution in the population:\n";
-    std::cout << "Solution: " << bestStart << "\nfitness: " << bestFitness << "\nSize: " << bestSolnStart.size() << " strips.\n\n";
+    //std::cout << "START - Best solution in the population:\n";
+    //std::cout << "Solution: " << bestStart << "\nfitness: " << bestFitness << "\nSize: " << bestSolnStart.size() << " strips.\n\n";
 
     bestEnd = bestStart;
 
@@ -145,118 +150,84 @@ int main(int argc, char** argv){
            allItems, populationSum, population, qualityStripsSum, qualityStrips, qualityItems);
     }
 
-    /*std::cout << "qualityStrips - Strips that are in qualityStrips set:\n";
-    for(i = 0; i < qualityStrips.size(); ++i){
-        for(j = 0; j < qualityStrips[i].size(); ++j){
-            std::cout << qualityStrips[i][j] << " ";
+    std::cout << "feasiblePacking:\n";
+    for(const auto& subVec : feasiblePacking){
+        for(const auto& v : subVec){
+            std::cout << v << " ";
         }
         std::cout << std::endl;
     }
-    std::cout << std::endl << std::endl;*/
+    std::cout << std::endl;
 
-    /*std::cout << "qualityStripSum - Sum of all items on each strip:\n";
-    for(i = 0; i < qualityStripsSum.size(); ++i){
-        std::cout << qualityStripsSum[i] << " ";
-    }
-    std::cout << std::endl << std::endl;*/
-
-    /*std::cout << "qualityItems - Number of times item appears in qualityStrips set\n";
-    for(i = 0; i < qualityItems.size(); ++i){
-        std::cout << qualityItems[i] << " ";
-    }
-    std::cout << std::endl << std::endl;*/
-
-    //Rows of matrix are strips, columns are items
-    std::vector<std::vector<int> > dlxMatrix(qualityStrips.size());
-
-    for(i = 0; i < qualityStrips.size(); ++i){
-        for(j = 0; j < qualityStrips[i].size() -1; j+=2){
-            dlxMatrix[i].push_back(allItems[qualityStrips[i][j]][qualityStrips[i][j+1]]);
-        }
-        std::sort(dlxMatrix[i].begin(), dlxMatrix[i].end());
-
-    }
-    /*std::vector<int> temp;
-    for(i = 0; i < numItem; ++i){
-        temp.push_back(i);
-    }
-
-    dlxMatrix.insert(dlxMatrix.begin(), temp);*/
-
-    std::cout << "Size of dlxMatrix: " << dlxMatrix.size() << std::endl;
-
-    std::set<std::vector<int> > setQS;
-    for(i = 0; i < dlxMatrix.size(); ++i){
-        setQS.insert(dlxMatrix[i]);
-    }
-
-    std::cout << "Size of setQS: " << setQS.size() << std::endl;
-
-    std::set<std::vector<int> >::const_iterator itSet;
-    std::vector<int>::const_iterator itVector;
-
-    /*std::cout << "setQS:\n";
-    for(itSet = setQS.begin(); itSet != setQS.end(); ++itSet){
-        for(itVector = itSet->begin(); itVector != itSet->end(); ++itVector){
-            std::cout << *itVector << " ";
+    /*std::cout << "qualityStrips - Strips that are in qualityStrips set:\n";
+    for(const auto& subVec : qualityStrips){
+        for(const auto& v : subVec){
+            std::cout << v << " ";
         }
         std::cout << std::endl;
     }
     std::cout << std::endl;*/
 
-    std::ofstream ofs("settest2.txt");
+    /*std::cout << "qualityStripSum - Sum of all items on each strip:\n";
+    for(const auto& v : qualityStripsSum){
+        std::cout << v << " ";
+    }
+    std::cout << std::endl << std::endl;*/
+
+    /*std::cout << "qualityItems - Number of times item appears in qualityStrips set\n";
+    for(const auto& v : qualityItems){
+        std::cout << v << " ";
+    }
+    std::cout << std::endl <<std::endl;*/
+
+    //Rows of matrix are strips, columns are items
+    std::vector<std::vector<int> > dlxMatrix(qualityStrips.size());
+
+    for(int i = 0; i < qualityStrips.size(); ++i){
+        for(int j = 0; j < qualityStrips[i].size() -1; j+=2){
+            dlxMatrix[i].push_back(allItems[qualityStrips[i][j]][qualityStrips[i][j+1]]);
+        }
+        std::sort(dlxMatrix[i].begin(), dlxMatrix[i].end());
+
+    }
+
+    //std::cout << "Size of dlxMatrix: " << dlxMatrix.size() << std::endl;
+
+    std::set<std::vector<int> > setQS;
+    for(int i = 0; i < dlxMatrix.size(); ++i){
+        setQS.insert(dlxMatrix[i]);
+    }
+
+    //std::cout << "Size of setQS: " << setQS.size() << std::endl;
+
+    std::set<std::vector<int> >::const_iterator itSet;
+    std::vector<int>::const_iterator itVector;
+
+    std::ofstream ofs("test.txt");
     if(!ofs){
         std::cerr << "[ERROR]: Cannot write to file." << std::endl;
         exit(1);
     }
-    std::vector<int> temp;
-    for(i = 0; i < numItem; ++i){
+    std::vector<int> temp2;
+    for(int i = 0; i < numItem; ++i){
         temp.push_back(i);
     }
     std::copy(temp.begin(), temp.end(), std::ostream_iterator<int>(ofs, " "));
     ofs << std::endl;
+    //std::cout << "setQS:\n;
     for(itSet = setQS.begin(); itSet != setQS.end(); ++itSet){
         for(itVector = itSet->begin(); itVector != itSet->end(); ++itVector){
             ofs << *itVector << " ";
+            //std::cout << *itVector << " ";
         }
         ofs << std::endl;
+        //std::cout << std::endl
     }
     ofs.close();
+    std::cout << std::endl;
 
-    /*std::vector<int> temp;
-    for(i = 0; i < numItem; ++i){
-        temp.push_back(i);
-    }
-
-    dlxMatrix.insert(dlxMatrix.begin(), temp);
-
-    std::cout << "dlxMatrix:\n";
-    for (i = 0; i < dlxMatrix.size(); ++i){
-        for(j = 0; j < dlxMatrix[i].size(); ++j){
-            std::cout << dlxMatrix[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl << std::endl;
-
-
-    std::ofstream ofs("outputtest.txt");
-    if(!ofs){
-        std::cerr << "[ERROR]: Cannot write to file." << std::endl;
-        exit(1);
-    }
-    for(i = 0; i < dlxMatrix.size(); ++i){
-        std::copy(dlxMatrix[i].begin(), dlxMatrix[i].end(), std::ostream_iterator<int>(ofs, " "));
-        ofs << std::endl;
-    }
-    ofs.close();*/
-
-
-
-
-
-    std::cout << "END - Best solution in the population:\n";
-    std::cout << "Solution: " << bestEnd << "\nfitness: " << bestFitness << "\nSize: " << population[bestEnd].size() << " strips." << std::endl << std::endl;
+    //std::cout << "END - Best solution in the population:\n";
+    //std::cout << "Solution: " << bestEnd << "\nfitness: " << bestFitness << "\nSize: " << population[bestEnd].size() << " strips." << std::endl << std::endl;
 
 
 }//END INT MAIN
